5e31a96c2fb106a4eadf728515f55a4e47633621
feather <1034080243@qq.com>
2017-10-17 23:54:45 +0800

    homework

--- selpg/README.md
+++ selpg/README.md
@@ -9,51 +9,65 @@
 3. golang stdin 读，参考这里
 4. 应用程序只能修改自己的环境变量，不能修改父程序的环境变量
 
+
+
 ## 设计说明
 [c语言链接](https://www.ibm.com/developerworks/cn/linux/shell/clutil/selpg.c)
 
 ## 使用与测试结果
  - ./selpg -s1 -e1 file1
+
 该命令将把“input_file”的第 1 页写至标准输出（也就是屏幕），因为这里没有重定向或管道。由于没有指定行号，默认一页72行。
 截图1
 截图2
 
 -  ./selpg -s1 -e1 < file1
+
 截图4
 截图5
 
-./selpg -s-3 -e-2 -l3
-panic: ./selpg: invalid start page -3
+- ./selpg -s-3 -e-2 -l3
 
-./selpg -s-2 -e-1 -l3
-panic: ./selpg: invalid start page -2
+> panic: ./selpg: invalid start page -3
 
-./selpg -s1 -e1 -l3 "no"
+
+- ./selpg -s-2 -e-1 -l3
+
+> panic: ./selpg: invalid start page -2
+
+- ./selpg -s1 -e1 -l3 "no"
+
 USAGE: ./selpg -sstart_page -eend_page [ -f | -llines_per_page ] [ -ddest ] [ in_filename ]
 panic: ./selpg: input file "no" does not exist
 
 
-./selpg -s1 -e2 -l3 < file1
+- ./selpg -s1 -e2 -l3 < file1
+
 截图3
-the first file-1
+
+> the first file-1
 the first file-2
 the first file-3
 the first file-4
 the first file-5
 the first file-6
 
-./selpg -s1 -e1 file1 > outputfile
+- ./selpg -s1 -e1 file1 > outputfile
+
 截图6
-the first file-1
+
+> the first file-1
 the first file-2
 the first file-3
 ...
 the first file-100
 
-./selpg -s500 -e200 file1 > error_file
+- ./selpg -s500 -e200 file1 > error_file
+
 截图6
 
 - $ selpg -s10 -e20 input_file >output_file 2>/dev/null 
+
 错误信息被丢弃
 
 
---
+++ selpg/main-cankao.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+// Args is the struct returned by command line argument parser
+type Args struct {
+	Sources chan *ReadSrc
+	Dest    io.Writer
+	Filter  PageFilter
+	Pager   Pager
+}
+
+// Get returns the pointer to the result of parsed args
+func Get() *Args {
+	if args == nil {
+		args = parseArgs()
+	}
+	return args
+}
+
+// PageFilter receive the page number and the page content returns whether
+// the page is wanted or not, and whether it should be the last page
+type PageFilter func(int, []byte) (bool, bool)
+
+// ReadSrc specifies the name and reader of the source
+type ReadSrc struct {
+	Reader io.Reader
+	Name   string
+	Next   chan bool
+}
+
+// Pager read string from the reader and combine them into pages
+type Pager func(io.Reader) chan []byte
+
+func pageFilter(start, end int) PageFilter {
+	return func(n int, content []byte) (contained, isLast bool) {
+		if n < start || n > end {
+			return
+		}
+		contained = true
+		if n == end {
+			isLast = true
+		}
+		return
+	}
+}
+
+func pageDelimiterPager(reader io.Reader) chan []byte {
+	channel := make(chan []byte)
+	coroutine := func() {
+		b := bufio.NewReader(reader)
+		for {
+			s, err := b.ReadBytes(byte('\f'))
+			s = append(s, byte('\n'))
+			channel <- s
+			if err != nil {
+				if err == io.EOF {
+					close(channel)
+					break
+				} else {
+					panic(fmt.Sprintf(
+						"Unexpected error when reading from reader: %s",
+						err.Error()))
+				}
+			}
+		}
+	}
+	go coroutine()
+	return channel
+}
+
+func fixLinePager(n uint) Pager {
+	return func(reader io.Reader) chan []byte {
+		channel := make(chan []byte)
+		coroutine := func() {
+			lineScan := bufio.NewScanner(reader)
+			for {
+				end := false
+				data := make([]byte, 0)
+				for i := uint(0); i < n; i++ {
+					if lineScan.Scan() {
+						line := lineScan.Bytes()
+						line = append(line, byte('\n'))
+						data = append(data, line...)
+					} else {
+						end = true
+						break
+					}
+				}
+				channel <- data
+				if end {
+					close(channel)
+					break
+				}
+			}
+		}
+		go coroutine()
+		return channel
+	}
+}
+
+func argumentError(reason string, usage bool) {
+	fmt.Fprintln(os.Stderr, reason)
+	if usage {
+		flag.Usage()
+	}
+	os.Exit(2)
+}
+
+var args *Args
+
+func pageFilterOrExit(start, end int) PageFilter {
+	if start < 0 || end < 0 {
+		argumentError("both start and end argument should be positive", true)
+	}
+	if start > end {
+		argumentError("start should be less than end", true)
+	}
+	return pageFilter(start, end)
+}
+
+func getSources() chan *ReadSrc {
+	channel := make(chan *ReadSrc)
+	next := make(chan bool)
+
+	coroutine := func() {
+		if len(flag.Args()) == 0 {
+			channel <- &ReadSrc{Reader: os.Stdin}
+			close(channel)
+			return
+		}
+		for _, f := range flag.Args() {
+			file, err := os.Open(f)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				continue
+			}
+			// equivalent: name := len(flag.Args()) == 1 ? "" : f
+			var name string
+			if len(flag.Args()) != 1 {
+				name = f
+			}
+			channel <- &ReadSrc{Name: name, Reader: file, Next: next}
+			b := <-next // waiting user handing over control
+			file.Close()
+			if !b {
+				break
+			}
+		}
+		close(channel)
+	}
+	go coroutine()
+	return channel
+}
+
+func getDestOrExit(dest string) io.Writer {
+	dest = strings.TrimSpace(dest)
+	if dest == "" {
+		return os.Stdout
+	}
+	spaces, err := regexp.Compile("\\s+")
+	if err != nil {
+		panic("what spaces regex compile fail??")
+	}
+	cmds := spaces.Split(dest, -1)
+	cmd := exec.Command(cmds[0], cmds[1:]...)
+	writer, err := cmd.StdinPipe()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Start()
+	if err != nil {
+		argumentError(err.Error(), false)
+	}
+	return writer
+}
+
+func getPagerOrExit(lines uint, pageSep bool) Pager {
+	if pageSep {
+		return pageDelimiterPager
+	}
+	return fixLinePager(lines)
+}
+
+func parseArgs() *Args {
+	start := flag.Int("s", -1, "the starting index of page number (mandatory)")
+	end := flag.Int("e", -1, "the ending index of page number (mandatory)")
+	pageSeperator := flag.Bool("f", false, "use \\f to seperate pages")
+	pageLines := flag.Uint("l", 72, "number of lines for each page")
+	dest := flag.String(
+		"d", "", "a destination command that receives the output,"+
+			" if it's not specified, output is printed to stdout")
+
+	flag.Parse()
+	filter := pageFilterOrExit(*start, *end)
+	sources := getSources()
+	destWriter := getDestOrExit(*dest)
+	pager := getPagerOrExit(*pageLines, *pageSeperator)
+	return &Args{
+		Sources: sources,
+		Filter:  filter,
+		Dest:    destWriter,
+		Pager:   pager,
+	}
+}
+
+func errorExit(reason string) {
+	fmt.Fprintln(os.Stderr, reason)
+	os.Exit(1)
+}
+
+func selpg(a *Args) {
+	//
+	for source := range a.Sources {
+		if len(source.Name) != 0 {
+			a.Dest.Write([]byte(fmt.Sprintf("%s:\n", source.Name)))
+		}
+		n := 0
+		for page := range a.Pager(source.Reader) {
+			// 页码选择
+			yes, last := a.Filter(n, page)
+			if yes {
+				_, err := a.Dest.Write(page)
+				if err != nil {
+					errorExit(fmt.Sprintf(
+						"Unexpected error on write: %s", err.Error()))
+				}
+			}
+			if last {
+				break
+			}
+			n++
+		}
+		if source.Next != nil {
+			source.Next <- true
+		}
+	}
+}
+
+func main() {
+	args := Get()
+	selpg(args)
+}
